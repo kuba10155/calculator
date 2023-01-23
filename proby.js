f2ad1ff0e63ccfa3973c8cc08dd7cce989ce5222
kuba10155 <j.zborowski24@gmail.com>
2023-01-23 18:38:15 +0100

    reworked

--- proby.js
+++ proby.js
@@ -1,28 +1,45 @@
 
-let num1 = 8
-let num2 = 2
 let answer = 0
-document.getElementById("num1-el").textContent = num1
-document.getElementById("num2-el").textContent = num2
-
+let num1 = 0
+let num2 = 0
 let span = document.getElementById("sum-el")
 
+const addBtn = document.getElementById('add-btn')
+const subBtn = document.getElementById('sub-btn')
+const divBtn = document.getElementById('div-btn')
+const mulBtn = document.getElementById('mul-btn')
+
+addBtn.addEventListener('click', add)
+subBtn.addEventListener('click', subtract)
+divBtn.addEventListener('click', divide)
+mulBtn.addEventListener('click', multiply)
+
+
+function getNumbers() {
+  num1 = Number(document.getElementById('num1-el').value)
+  num2 = Number(document.getElementById('num2-el').value)
+}
+
 function add() {
+  getNumbers()
   answer = num1 + num2
-  span.textContent = "Sum: " + answer
+  span.textContent = "Answer: " + answer
 }
 
 function subtract() {
+  getNumbers()
   answer = num1 - num2
-  span.textContent = "Sum: " + answer
+  span.textContent = "Answer: " + answer
 }
 
 function divide() {
+  getNumbers()
   answer = num1 / num2
-  span.textContent = "Sum: " + answer
+  span.textContent = "Answer: " + answer
 }
 
 function multiply() {
+  getNumbers()
   answer = num1 * num2
-  span.textContent = "Sum: " + answer
+  span.textContent = "Answer: " + answer
 }
